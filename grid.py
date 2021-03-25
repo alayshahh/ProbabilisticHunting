82ed3cb183ce0c7771696dbe3ad697b9287e1178
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2021-03-25 16:57:27 -0400

    [ ADD ] added grid generation and rendering
    Also included notebook for examples

---
+++ grid.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import enum
+from random import randint
+from PIL import Image
+import numpy as np
+
+
+class TERRAIN_TYPES(enum.Enum):
+    FLAT = 0
+    HILLY = 1
+    FORESTED = 2
+    MAZE = 3  # Complex Maze of Caves and Tunnels
+
+
+# False negative dictionary
+#   Maps terrain type to the corresponding false negative rate
+#   We define false negative rate by the following:
+#
+#   P(Target not found in Cell i | Targent)
+PROBABILITY_DICT = {
+    TERRAIN_TYPES.FLAT: 0.1,
+    TERRAIN_TYPES.HILLY: 0.3,
+    TERRAIN_TYPES.FORESTED: 0.7,
+    TERRAIN_TYPES.MAZE: 0.9
+}
+
+
+def generate_grid(dim: int = 50):
+    """
+    Generate a grid of a particular dimension, and randomly assign each
+    a terrain type. We assign cells based on the following probability dist
+        Distribution:
+            TERRAIN_TYPES.FLAT = 0.25
+            TERRAIN_TYPES.HILLY = 0.25
+            TERRAIN_TYPES.FORESTED = 0.25
+            TERRAIN_TYPES.MAZE = 0.25
+    """
+    # initialize our grid to flat by default
+    grid = [[TERRAIN_TYPES.FLAT for _ in range(dim)] for _ in range(dim)]
+
+    # To achieve our distrobution, we are going to use a random number
+    # generator, and branch accordingly
+
+    for i in range(dim):
+        for j in range(dim):
+            val = randint(0, 3)
+            grid[i][j] = TERRAIN_TYPES(randint(0, 3))
+
+    return grid
+
+
+def create_image_from_grid(grid: list, factor: int = 1):
+    """
+    Given a grid that came from generate_grid(), return
+    a PIL Image object that can be either saved as a file.
+
+    factor will scale the image by a factor of len(grid)
+    """
+
+    # convert terrain types to color codes
+    RGB_VALUES = {
+        TERRAIN_TYPES.FLAT: (255, 255, 255),
+        TERRAIN_TYPES.HILLY: (217, 217, 217),
+        TERRAIN_TYPES.FORESTED: (106, 168, 79),
+        TERRAIN_TYPES.MAZE: (67, 67, 67)
+    }
+
+    dim = len(grid)
+    image_array = np.zeros((dim*factor, dim*factor, 3), dtype=np.uint8)
+    for i in range(dim):
+        for j in range(dim):
+            r, g, b = RGB_VALUES[grid[i][j]]
+            for ki in range(factor):
+                for kj in range(factor):
+                    image_array[i*factor + ki, j *
+                                factor + kj, 0] = r  # red channel
+                    image_array[i*factor + ki, j*factor +
+                                kj, 1] = g  # green channel
+                    image_array[i*factor + ki, j *
+                                factor + kj, 2] = b  # blue channel
+
+    img = Image.fromarray(image_array, 'RGB')
+    return img
