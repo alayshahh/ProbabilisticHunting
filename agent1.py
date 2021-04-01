bf6aacb5b00fe0a34d8d8c2129bf6b5bd3a4aac0
Alay Shah <alayshah9@icloud.com>
2021-04-01 12:35:58 -0400

    [ FIX ] fixed the methods for agent1 Fixed some of the probability calculations and fixed the way we score to how the assignment describes score.

--- agent1.py
+++ agent1.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from grid import Cell
+from pprint import pprint
+import time
+import random
 
 
 def run(grid, belief_matrix, probability_dict):
+    '''
+    Moves through the grid and looks at cells with the highest probability of having the target to search. 
+    Does not account for distance between cells.
+    '''
+
     score = 0
     found = False
-
     dim = len(grid)
+
+    # we will start at 0,0 since all probs are equal
+    prev_max = (0, 0)
 
     while True:
         # find Cell with the highest probability
-
         highest_prob, max_prob_loc = 0, (0, 0)
         for i in range(dim):
             for j in range(dim):
@@ -18,36 +28,58 @@ def run(grid, belief_matrix, probability_dict):
                     highest_prob = belief_matrix[i][j]
                     max_prob_loc = i, j
 
+        old_i, old_j = prev_max
         i, j = max_prob_loc
-        score += 1
 
-        found = grid[i][j].search()
-        if found:
+        # calculate the moves made to get to the new max prob cell i.e manhattan distance from last max to cur max cell
+        dist_travelled = abs(i - old_i) + abs(j - old_j)
+        # score = total distance travelled + total # of searches
+        score += (1+dist_travelled)
+        prev_max = max_prob_loc
+
+        max_prob_cell = grid[i][j]
+        # search the cell
+        found = max_prob_cell.search()
+
+        if found:  # if the target is found we are done
             break
 
-        # recompute belief matrix
+        # recompute belief matrix if we havent found the target
+        # belief for max_cell =  P(in max cell| not found in max cell)
+        # = P(in max cell)*P(not found|in max cell)/ P(not found)
+        # numerator = P(in max cell) * P(not found in max cell| in cell)  = belief[i,j]* prob of not finding (given)
         belief_given_obs_numerator = belief_matrix[i][j] * \
-            (1 - probability_dict[grid[i][j].terrain_type])
-        belief_given_obs_denominator = belief_matrix[i][j] * \
-            probability_dict[grid[i][j].terrain_type] + (1-belief_matrix[i][j])
+            probability_dict[max_prob_cell.terrain_type]
 
+        # denominator = P(not found in max cell)
+        # = P(in max cell) P(not found|in max cell) + P(not in max cell)P(not found| not in max cell)
+        # = belief[i,j]* given prob of not finding + [1 - belief[i,j]]*1 (no false positives)
+        belief_given_obs_denominator = belief_matrix[i][j] * \
+            probability_dict[max_prob_cell.terrain_type] + \
+            (1 - belief_matrix[i][j])
+        # update value in matrix
         belief_matrix[i][j] = belief_given_obs_numerator / \
             belief_given_obs_denominator
-
+        # update the rest of our beielf matrix
         update_belief_matrix(grid, belief_matrix, (i, j),
                              belief_given_obs_denominator, probability_dict)
 
-        print(score)
+        # print(score)
 
     return score
 
 
 def update_belief_matrix(grid, belief_matrix, max_location, denominator, probability_dict):
+    ''' 
+    Update rest of beief belief_matrix
+    '''
+    # P(in cur cell| not found in max cell) = P(in cur cell && not found in max cell)/ P (not found in max cell)
+    # we have already computed the denominator from before, so we do not need to redo it
+    # numerator:  P(in cur cell && not found in max cell) = P(in cur cell)P(not found in max cell| in cur cell) = P(in cur cell)*1 (no false positives)
+    # = beief[cur_i, cur_j]/ denominator
     dim = len(belief_matrix)
     for i in range(dim):
         for j in range(dim):
-            if (i, j) != max_location:
-                numerator = belief_matrix[i][j] * \
-                    probability_dict[grid[i][j].terrain_type]
-
+            if (i, j) != max_location:  # do not update the max cell
+                numerator = belief_matrix[i][j]
                 belief_matrix[i][j] = numerator / denominator
