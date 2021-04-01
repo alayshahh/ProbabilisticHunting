1668b9ba8852e5b6fac488e03f6482653358f087
Alay Shah <alayshah9@icloud.com>
2021-04-01 14:11:33 -0400

    Merge conflict resovled

--- grid.py
+++ grid.py
@@ -95,12 +95,9 @@ if __name__ == '__main__':
     belief_matrix = [[1 / (dim**2) for _ in range(dim)] for _ in range(dim)]
 
     # Agent 1
-<<<<<<< HEAD
     agent_1_score = agent1.run(grid, belief_matrix)
     print(agent_1_score)
     # agent_2_score = agent2.run(grid, belief_matrix)
     # print(agent_2_score)
-=======
     agent_1_score = agent1.run(grid, belief_matrix, PROBABILITY_DICT)
     print("Found agent. Took " + str(agent_1_score) + " steps")
->>>>>>> 07068ae368e4b0a83833dcf1c28426c1eb2de5b8
